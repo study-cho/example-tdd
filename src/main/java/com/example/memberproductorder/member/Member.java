d728178e07183fadf1be384c8b910600ca98f3d5
study-cho <dmcho.ex@gmail.com>
2023-04-21 17:17:22 +0900

    회원등록 - JPA 적용

--- src/main/java/com/example/memberproductorder/member/Member.java
+++ src/main/java/com/example/memberproductorder/member/Member.java
@@ -1,12 +1,22 @@
 package com.example.memberproductorder.member;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final String memberId;
-    private final String memberName;
-    private final MemberGrade memberGrade;
+    private String memberId;
+    private String memberName;
+    private MemberGrade memberGrade;
 
     public Member(String memberId, String memberName, MemberGrade memberGrade) {
         Assert.hasText(memberId, "회원 아이디는 필수입니다.");
@@ -15,14 +25,6 @@ class Member {
         this.memberId = memberId;
         this.memberName = memberName;
         this.memberGrade = memberGrade;
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 
 }
--- src/main/java/com/example/memberproductorder/member/MemberRepository.java
+++ src/main/java/com/example/memberproductorder/member/MemberRepository.java
@@ -1,17 +1,10 @@
 package com.example.memberproductorder.member;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Repository
-class MemberRepository {
-    private final Map<Long, Member> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(Member member) {
-        member.assignId(++sequence);
-        persistence.put(member.getId(), member);
-    }
+interface MemberRepository extends JpaRepository<Member, Long> {
 }
