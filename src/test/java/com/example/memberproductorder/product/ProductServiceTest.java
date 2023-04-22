b6b3d672f1f89e6ecc9315367264f27b21603bcd
study-cho <dmcho.ex@gmail.com>
2023-04-22 13:58:07 +0900

    상품조회 - POJO 구현

--- src/test/java/com/example/memberproductorder/product/ProductServiceTest.java
+++ src/test/java/com/example/memberproductorder/product/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.memberproductorder.product;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,10 @@ import org.springframework.util.Assert;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ProductServiceTest {
 
@@ -24,10 +29,44 @@ class ProductServiceTest {
     @Test
     @DisplayName("상품등록")
     void addProduct() {
+        AddProductRequest request = addProductRequest_create();
+        productService.addProduct(request);
+    }
+
+    private static AddProductRequest addProductRequest_create() {
         String productName = "상품1";
         int productPrice = 1000;
         AddProductRequest request = new AddProductRequest(productName, productPrice);
-        productService.addProduct(request);
+        return request;
+    }
+
+    @Test
+    @DisplayName("상품조회")
+    void findProduct() {
+        //given
+        productService.addProduct(addProductRequest_create());
+        final Long productId = 1L;
+
+        //when
+        GetProductResponse response = productService.findProduct(productId);
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.productName()).isEqualTo("상품1");
+    }
+
+    @Test
+    @DisplayName("상품조회_wrongId")
+    void findProduct_throw() {
+        //given
+        final Long wrongId = 1L;
+
+        //when
+        //then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> productService.findProduct(wrongId));
+        assertThat(exception.getMessage()).isEqualTo("잘못된 상품 아이디입니다.");
+
     }
 
     private record AddProductRequest(String productName, int productPrice) {
@@ -48,6 +87,16 @@ class ProductServiceTest {
             Product product = new Product(request.productName(), request.productPrice());
 
             productPort.addProduct(product);
+        }
+
+        public GetProductResponse findProduct(Long productId) {
+            Product product = productPort.findProduct(productId);
+
+            return new GetProductResponse(
+                    product.getId(),
+                    product.getProductName(),
+                    product.getProductPrice()
+            );
         }
     }
 
@@ -70,10 +119,19 @@ class ProductServiceTest {
         public Long getId() {
             return id;
         }
+
+        public String getProductName() {
+            return productName;
+        }
+
+        public int getProductPrice() {
+            return productPrice;
+        }
     }
 
     private interface ProductPort {
         void addProduct(Product product);
+        Product findProduct(Long productId);
     }
 
     class ProductAdapter implements ProductPort {
@@ -87,6 +145,12 @@ class ProductServiceTest {
         public void addProduct(Product product) {
             productRepository.addProduct(product);
         }
+
+        @Override
+        public Product findProduct(Long productId) {
+            return productRepository.findProduct(productId)
+                    .orElseThrow(() -> new IllegalArgumentException("잘못된 상품 아이디입니다."));
+        }
     }
 
 
@@ -98,5 +162,16 @@ class ProductServiceTest {
             product.assignId(++sequence);
             persistence.put(product.getId(), product);
         }
+
+        public Optional<Product> findProduct(Long productId) {
+            return Optional.ofNullable(persistence.get(productId));
+        }
+    }
+
+    private record GetProductResponse(
+            Long id,
+            String productName,
+            int productPrice
+    ) {
     }
 }
